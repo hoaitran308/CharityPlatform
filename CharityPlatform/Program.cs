4148daa56cd52a7a4056b0ac6b62c2753c2e9697
hoaitran308 <tranvanhoai.tt4@gmail.com>
2022-06-10 18:57:13 +0700

    implement identity

--- CharityPlatform/Program.cs
+++ CharityPlatform/Program.cs
@@ -1,9 +1,15 @@
 using CharityPlatform.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using CharityPlatform.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'MyDbContextConnection' not found.");
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<MyDbContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -31,5 +37,6 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.UseAuthentication();;
 
 app.Run();
